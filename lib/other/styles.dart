469bbcdee9339e323d348b4dabeb5f1a94d9560a
vraj <vrajbhatt27@gmail.com>
2021-12-18 12:17:44 +0530

    BG Color and bgt color changed to google keep theme

--- lib/other/styles.dart
+++ lib/other/styles.dart
@@ -19,16 +19,30 @@ import 'package:flutter/painting.dart';
 //   static const Color accentColor2 = Color(0xFFffa500); //ef8354 , F92D07
 // }
 
+// abstract class AppColors {
+//   /// Dark background color.
+//   static const Color backgroundColor = Color(0xFF202124);
+//   static const Color bgtColor = Color(0xFF525355);
+
+//   /// Color used for cards and surfaces.
+//   static Color cardColor = Color(0xFFb5b7cc).withOpacity(0.9);
+//   static Color popUpCardColor = Color(0xFFFFCC99);
+
+//   /// Accent color used in the application.
+//   static const Color accentColor = Color(0xFFea5e33);
+//   static const Color accentColor2 = Color(0xFFffa500);
+// }
+
 abstract class AppColors {
   /// Dark background color.
-  static const Color backgroundColor = Color(0xFF202124); 
-  static const Color bgtColor = Color(0xFF525355); 
+  static const Color backgroundColor = Color(0xFF1F1F1F);
+  static const Color bgtColor = Color(0xFF2C2D31); //0xFF2E2F33
 
   /// Color used for cards and surfaces.
-  static Color cardColor = Color(0xFFb5b7cc).withOpacity(0.9); 
-  static Color popUpCardColor = Color(0xFFFFCC99); 
+  static Color cardColor = Color(0xFFb5b7cc).withOpacity(0.9);
+  static Color popUpCardColor = Color(0xFFFFCC99);
 
   /// Accent color used in the application.
-  static const Color accentColor = Color(0xFFea5e33); 
-  static const Color accentColor2 = Color(0xFFffa500); 
+  static const Color accentColor = Color(0xFFea5e33);
+  static const Color accentColor2 = Color(0xFFffa500);
 }
